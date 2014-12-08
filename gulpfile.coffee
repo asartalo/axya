5dc923e9f092c856d59f861447a30e074d15b045
Wayne Duran <asartalo@gmail.com>
2014-12-09 02:11:51 +0800

    Removing extra gulpfile code.

--- gulpfile.coffee
+++
@@ -1,5 +0,0 @@
-requireDir = require('require-dir')
-
-# Require all tasks in gulp/tasks, including subfolders
-requireDir('./gulp/tasks', { recurse: true })
-
--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,5 @@
 require('coffee-script/register');
-require('./gulpfile.coffee');
+
+// Require all tasks in gulp/tasks, including subfolders
+require('require-dir')('./gulp/tasks', { recurse: true });
+
