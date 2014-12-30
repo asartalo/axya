f90d0ae4136331e4fd24b8876f07630fbaac76ab
Wayne Duran <asartalo@gmail.com>
2014-12-30 11:39:44 +0800

    Add port command line flag for server.

--- serve/serve.go
+++ serve/serve.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/asartalo/axya"
+	"github.com/codegangsta/cli"
 	"github.com/gorilla/mux"
 	"net/http"
 	"os"
@@ -10,8 +11,7 @@ import (
 	"syscall"
 )
 
-func main() {
-
+func startServer(port int) {
 	r := mux.NewRouter()
 	injector := axya.Injector(http.FileServer(http.Dir("./builds/development")))
 
@@ -19,7 +19,7 @@ func main() {
 	r.PathPrefix("/").Handler(injector)
 	http.Handle("/", r)
 
-	fmt.Println("Starting Server")
+	fmt.Println(fmt.Sprintf("Starting Server. Listening at port %d", port))
 	go func() {
 		c := make(chan os.Signal, 10)
 		signal.Notify(c, os.Interrupt, syscall.SIGTERM)
@@ -27,5 +27,24 @@ func main() {
 		fmt.Println("\nStopping Server...")
 		os.Exit(0)
 	}()
-	http.ListenAndServe(":9876", nil)
+	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+}
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "server"
+	app.Usage = "Starts the Axya server"
+	app.Action = func(c *cli.Context) {
+		startServer(c.Int(`port`))
+	}
+
+	app.Flags = []cli.Flag{
+		cli.IntFlag{
+			Name:  "port, p",
+			Value: 9876,
+			Usage: "port where the server will listen to",
+		},
+	}
+
+	app.Run(os.Args)
 }
